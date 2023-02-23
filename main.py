204f164b4317e42eddd32060ff7b813d2eb91c40
JCB5150 <38227743+jakebauer5150@users.noreply.github.com>
2023-02-23 12:08:12 -0700

    test cases
    Added some example test cases

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from canvasapi import Canvas
 import os
 import json
 
-
 # Canvas API URL
 API_URL = "https://canvas.instructure.com/"
 # Canvas API key
@@ -19,8 +18,9 @@ def get_config_files():
             files.append(file)
     return files
 
+
 def get_submissions(config, config_file):
-    
+
     try:
         course = canvas.get_course(config["CANVAS"]["COURSE_ID"])
     except:
@@ -34,22 +34,27 @@ def get_submissions(config, config_file):
 
     submissions = assignment.get_submissions()
 
-    # get sumbissions and test them
+    # get submissions and test them
     for submission in submissions:
         if submission.workflow_state == "submitted":
             # print("Testing submission: " + str(Canvas.get_user(canvas, 37434307).name))
             file = Canvas.get_file(canvas, submission.attachments[0])
-            test_submission(file, config)
-
-def test_submission(file, config):
-    test_file = open(config["FUNCTION_TESTS"]["TEST_FILE"]), "r"
+            check_submission(file, config)
 
 
-# right now progress bar is being used for each config file. Soon it will be used for users submissions once we get testing working
+def check_submission(file, config):
+    # TODO: get and open file here
+    for test in config["TESTS"]:
+        function = test["TEST_FUNCTION"]
+
+
+# right now progress bar is being used for each config file.
+# Soon it will be used for users submissions once we get testing working
 def progress_bar(progress, total, file_name):
-    perecent = 100 * (progress / float(total))
-    bar = "▮" * int(perecent) + "-" * (100 - int(perecent))
-    print(f"\r|{bar}| {perecent:.2f}% {file_name}", end="\r")
+    percent = 100 * (progress / float(total))
+    bar = "▮" * int(percent) + "-" * (100 - int(percent))
+    print(f"\r|{bar}| {percent:.2f}% {file_name}", end="\r")
+
 
 def main():
     config_files = get_config_files()
@@ -60,10 +65,10 @@ def main():
             try:
                 get_submissions(config, config_file)
             except Exception as e:
-                print("Error loading config file: " + config_file + " skipping...")
+                print("Error loading config file: " + config_file + "; skipping...")
                 print(e)
         progress_bar(config_files.index(config_file) + 1, len(config_files), config_file)
-            
-            
+
+
 if __name__ == "__main__":
     main()
--- scripts/test_assignment.py
+++ scripts/test_assignment.py
@@ -1,3 +1,30 @@
-class Test_assignment():
-    def add_working(a, b):
-        return a + b
+class TestAssignment:
+    pass
+
+
+def add(a, b):
+    return a + b
+
+
+def switch(a, b):
+    return b, a
+
+
+def factorial(num):
+    if num == 0:
+        return 1
+    return num * factorial(num - 1)
+
+
+def print_twice(string):
+    print(string)
+    print(string)
+
+
+def get_user_input():
+    string = str(input("Type something: "))
+    return string
+
+
+def test_from_file():
+    print("This is a test case.")
