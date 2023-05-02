22777817e8552070f9f4baeb56dc6b15d67b4e77
QuinntonP <QuinntonParchman@gmail.com>
2023-05-02 11:28:34 -0600

    test

---
+++ Test Assignments/test_case1.py
@@ -0,0 +1,16 @@
+# subtract does not work
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y + 1
+
+def divide(x, y):
+    return x / y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
