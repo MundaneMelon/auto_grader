ffe99d19078d69736ca931b810dfa50c5e14d2d9
DonovanMMM <124104944+DonovanMMM@users.noreply.github.com>
2023-05-07 19:51:51 -0600

    String Assignment test files

---
+++ Test Assignments/string_assignment.py
@@ -0,0 +1,21 @@
+def hello_name(name=""):
+    return "Hello " + name + "!"
+
+
+def make_abba(a="", b=""):
+    return a + b + b + a
+
+
+def make_tags(tag="", string=""):
+    return f"<{tag}>{string}</{tag}>"
+
+
+def make_out_word(brackets="", word=""):
+    for i in range(len(brackets)):
+        if brackets[i] == ">" or brackets[i] == "}" or brackets[i] == "]":
+            brackets = brackets[:i] + word + brackets[i:]
+            return brackets
+
+
+def extra_end(string=""):
+    return string[len(string) - 2:] * 3
